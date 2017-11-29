1cdef4eb32cbf723807881355ff9bc8b182a08b8
parkpoom <parkpoom.d@gmail.com>
2017-11-29 22:36:03 +0700

    Test the DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -43,7 +43,7 @@ app.get('/todos/:id', (req, res) => {
       return res.status(404).send();
     }
 
-    res.send(todo);
+    res.send({todo});
   }).catch((e) => {
     res.status(400).send();
   });
@@ -61,7 +61,7 @@ app.delete('/todos/:id', (req, res) => {
       return res.status(404).send();
     }
 
-    res.send(todo);
+    res.send({todo});
   }).catch((e) => {
     res.status(400).send();
   });
